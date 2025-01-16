0d45c7967e0e20cdaa4f77910fa1fd5515caaced
Sean Nam <blt24q4@gmail.com>
2025-01-16 11:18:58 +0900

    checkEmail JavaScript

--- src/main/java/himedia/myportal/controllers/UsersController.java
+++ src/main/java/himedia/myportal/controllers/UsersController.java
@@ -1,6 +1,7 @@
 package himedia.myportal.controllers;
 
 import java.util.HashMap;
+
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import himedia.myportal.repositories.vo.UserVo;
 import himedia.myportal.services.UserService;
--- src/main/webapp/javascript/users.js
+++ src/main/webapp/javascript/users.js
@@ -1,7 +1,27 @@
 function checkEmail(event) {
 	// 가입 폼 #check-email을 누르면 email 텍스트 상자의 value가 중복되었는지
 	//	Ajax로 체크
-	alert("이메일 체크 이벤트");
+	const frm = event.target.form;
+	const url = event.target.getAttribute("data-target");
+	
+	const email = frm.email.value.trim();
+	
+	if (email.length === 0) {
+		alert("이메일을 입력해 주세요");
+		return;
+	}
+	
+	fetch(`${url}?email=$email`)
+	.then(response => {
+		console.log(response)
+		return response.json();
+	})
+	.then(data => {
+		alert(data);
+	})
+	.catch(error => {
+		console.error("Error:" + error);
+	})
 }
 
 window.addEventListener("load", (event) => {
